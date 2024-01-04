8e9f57fc0772ea23e8d6f4ded1362e31fcd1eb9b
Phu Nguyen Van <110567827+ViePhuNguyen@users.noreply.github.com>
2024-01-04 16:58:31 +0700

    problem breakdown

--- Labwork_1.py
+++ Labwork_1.py
@@ -1,1 +1,13 @@
-print("hello")
+""""                                            ///Problem breakdown///
+Input   
+        - Numbers of student in class (the size of list for initial input) 
+            //if want fix the numbers of student using tuple or not using list instead
+            + Student infor : "ID" , "Name" , "DoB" (using class) 
+        - Numbers of courses (list also)
+            + Courses information "ID" and "Name" 
+                   
+        - Select a course and input the mark for student in course
+Output - Show the courses
+       - Show the list students
+       - Show mark of student in given course
+"""
